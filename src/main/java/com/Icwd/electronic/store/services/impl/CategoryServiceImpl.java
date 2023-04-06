a453a19c9618978d0be1cf100473032b3565823d
SmitaBahade <smitabahade38@gmail.com>
2023-04-06 18:29:22 +0530

    CategoryServiceImpl 14 commit

--- src/main/java/com/Icwd/electronic/store/services/impl/CategoryServiceImpl.java
+++ src/main/java/com/Icwd/electronic/store/services/impl/CategoryServiceImpl.java
@@ -7,6 +7,7 @@ import com.Icwd.electronic.store.exceptions.ResourceNotFoundException;
 import com.Icwd.electronic.store.helper.Helper;
 import com.Icwd.electronic.store.repositories.CategoryRepository;
 import com.Icwd.electronic.store.services.CategoryService;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -14,7 +15,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-
+@Slf4j
 @Service
 public class CategoryServiceImpl implements CategoryService {
     @Autowired
@@ -24,14 +25,16 @@ public class CategoryServiceImpl implements CategoryService {
     private ModelMapper mapper;
     @Override
     public CategoryDto create(CategoryDto categoryDto) {
+        log.info("Initiating the dao call for create the Category data ");
         Category category   = mapper.map(categoryDto, Category.class);
         Category savedCategory = categoryRepository.save(category);
+        log.info("Complete the dao call for create the Category data :");
         return mapper.map(savedCategory,CategoryDto.class);
     }
 
     @Override
     public CategoryDto update(CategoryDto categoryDto, String categoryId) {
-
+        log.info("Initiating the dao call for update the Category data with id{}:",categoryId);
         //get category of given id
         Category category = categoryRepository.findById(categoryId).orElseThrow(() -> new ResourceNotFoundException("Category not found Exception !!"));
         //update category details
@@ -39,32 +42,35 @@ public class CategoryServiceImpl implements CategoryService {
         category.setDescription(categoryDto.getDescription());
         category.setCoverImage(categoryDto.getCoverImage());
         Category updatedCategory = categoryRepository.save(category);
+        log.info("Complete the dao call for update the Category data with id{}:",categoryId);
         return mapper.map(updatedCategory, CategoryDto.class);
     }
 
     @Override
     public void delete(String categoryId) {
-
+        log.info("Initiating the dao call for delete the Category data with id{}:",categoryId);
         //get category of given id
         Category category = categoryRepository.findById(categoryId).orElseThrow(() -> new ResourceNotFoundException("Category not found Exception !!"));
         categoryRepository.delete(category);
+        log.info("Complete the dao call for delete the Category data with id{}:",categoryId);
     }
 
     @Override
     public PageableResponse<CategoryDto> getAll(int pageNumber, int pageSize, String sortBy, String sortDir) {
-
-
+        log.info("Initiating the dao call for getAll the Category data ");
         Sort sort = (sortDir.equalsIgnoreCase("desc"))?(Sort.by(sortBy).descending()):(Sort.by(sortBy).ascending());
        Pageable pageable = PageRequest.of(pageNumber,pageSize,sort);
         Page<Category> page = categoryRepository.findAll(pageable);
         PageableResponse<CategoryDto> pageableResponse = Helper.getPageableResponse(page, CategoryDto.class);
+        log.info("Complete the dao call for getAll the Category data :");
         return pageableResponse;
     }
 
     @Override
     public CategoryDto get(String categoryId) {
+        log.info("Initiating the dao call for get the Category data with id{}:",categoryId);
         Category category = categoryRepository.findById(categoryId).orElseThrow(() -> new ResourceNotFoundException("Category not found Exception !!"));
-
+        log.info("Complete the dao call for get the Category data with id{}:",categoryId);
         return mapper.map(category,CategoryDto.class);
     }
 }
