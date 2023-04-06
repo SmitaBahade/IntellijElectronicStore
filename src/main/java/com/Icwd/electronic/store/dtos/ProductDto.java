af478becddfd456392e7343d1d6ca1f7f9cf72d0
SmitaBahade <smitabahade38@gmail.com>
2023-04-06 09:02:27 +0530

    my fourth commit

---
+++ src/main/java/com/Icwd/electronic/store/dtos/ProductDto.java
@@ -0,0 +1,24 @@
+package com.Icwd.electronic.store.dtos;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import java.util.Date;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ProductDto {
+    private String productId;
+    private String title;
+
+    private String description;
+    private int price;
+    private int discountedPrice;
+    private int quantity;
+
+    private Date addedDate;
+    private boolean live;
+    private boolean stock;
+}
