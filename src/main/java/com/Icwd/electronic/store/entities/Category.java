264b210098908dc2122877969bb3420894226c07
SmitaBahade <smitabahade38@gmail.com>
2023-04-16 19:23:00 +0530

    Category 18 commit

--- src/main/java/com/Icwd/electronic/store/entities/Category.java
+++ src/main/java/com/Icwd/electronic/store/entities/Category.java
@@ -1,9 +1,6 @@
 package com.Icwd.electronic.store.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -14,6 +11,7 @@ import javax.persistence.Table;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Entity
 @Table(name = "categoryies")
 public class Category {
