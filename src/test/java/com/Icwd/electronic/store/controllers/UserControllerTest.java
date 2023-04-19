d19472e1611aa8a9735363df37233757b406cb61
SmitaBahade <smitabahade38@gmail.com>
2023-04-19 23:43:16 +0530

    UserControllerTest 26 commit

--- src/test/java/com/Icwd/electronic/store/controllers/UserControllerTest.java
+++ src/test/java/com/Icwd/electronic/store/controllers/UserControllerTest.java
@@ -1,5 +1,6 @@
 package com.Icwd.electronic.store.controllers;
 
+import com.Icwd.electronic.store.dtos.PageableResponse;
 import com.Icwd.electronic.store.dtos.UserDto;
 import com.Icwd.electronic.store.entities.User;
 import com.Icwd.electronic.store.services.UserService;
@@ -13,11 +14,14 @@ import org.springframework.boot.test.autoconfigure.core.AutoConfigureCache;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 
 import java.awt.*;
+import java.util.Arrays;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -54,14 +58,44 @@ public class UserControllerTest {
 
         //actual request for url
         this.mockMvc.perform(MockMvcRequestBuilders.post("/users")
-                .contentType(PageAttributes.MediaType.APPLICATION_JSON)
-                .content(convertObjectToJsonString(user))
-                .accept(PageAttributes.MediaType.APPLICATION_JSON))
-                .andDo(print())
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.name").exists());
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(convertObjectToJsonString(user))
+                        .accept(MediaType.APPLICATION_JSON))
+                        .andDo(print())
+                        .andExpect(status().isCreated())
+                        .andExpect(jsonPath("$.name").exists());
     }
 
+    @Test
+    public void updateUserTest() throws Exception { // IT IS SECURED API
+        //  /users/{userId}+put+user data as json
+        String userId="123";
+        UserDto dto = this.mapper.map(user, UserDto.class);
+        Mockito.when(userService.updateUser(Mockito.any(),Mockito.anyString())).thenReturn(dto);
+
+        this.mockMvc.perform(MockMvcRequestBuilders.put("/users/"+ userId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(convertObjectToJsonString(user))
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").exists());
+    }
+//    @Test
+//    public void getAllUsersTest(){
+//        UserDto.builder().name(("Smita").email("smita@gmail.com").password("smita").about("Testing").build());
+//
+//        PageableResponse<UserDto> pageableResponse= new  PageableResponse<>();
+//        pageableResponse.setContent(Arrays.asList()
+//
+//        );
+//        pageableResponse.setLastPage(false);
+//        pageableResponse.setPageSize(10);
+//        pageableResponse.setPageNumber(100);
+//        pageableResponse.setTotalElements(1000);
+//
+//        Mockito.when(userService.getAllUser(Mockito.anyInt(),Mockito.any(),Mockito.anyString())).thenReturn(pageableResponse);
+//    }
     private String convertObjectToJsonString(Object user) {
         try{
             return new ObjectMapper().writeValueAsString(user);
