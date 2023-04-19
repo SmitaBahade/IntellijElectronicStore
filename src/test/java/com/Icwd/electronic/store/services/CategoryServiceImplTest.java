f5233b7205e7065a86827660ea03d33b4b6982e8
SmitaBahade <smitabahade38@gmail.com>
2023-04-19 23:41:21 +0530

    CategoryServiceImplTest 24 commit

--- src/test/java/com/Icwd/electronic/store/services/CategoryServiceImplTest.java
+++ src/test/java/com/Icwd/electronic/store/services/CategoryServiceImplTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -18,7 +19,7 @@ import org.springframework.data.domain.Pageable;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-
+@SpringBootTest
 public class CategoryServiceImplTest {
     @MockBean
     private CategoryRepository categoryRepository;
