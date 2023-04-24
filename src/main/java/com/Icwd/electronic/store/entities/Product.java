afb8dac189eb89d94a5d6cb1083b870ac1c9ba30
SmitaBahade <smitabahade38@gmail.com>
2023-04-24 11:04:55 +0530

    Product.java 30 commit

--- src/main/java/com/Icwd/electronic/store/entities/Product.java
+++ src/main/java/com/Icwd/electronic/store/entities/Product.java
@@ -2,10 +2,7 @@ package com.Icwd.electronic.store.entities;
 
 import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Date;
 @Getter
 @Setter
@@ -27,5 +24,8 @@ public class Product {
     private Date addedDate;
     private boolean live;
     private boolean stock;
-
+    private String productImageName;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "category_id")
+    private Category category;
 }
